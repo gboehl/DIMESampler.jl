de4d94250a00290a3c76586ccc4f672abf0c1111
Gregor Boehl <admin@gregorboehl.com>
2024-05-20 11:14:21 +0200

    rename parameter

--- src/DIMESampler.jl
+++ src/DIMESampler.jl
@@ -11,7 +11,7 @@ using Distributions, ProgressBars, Printf, LinearAlgebra, StatsFuns
 export RunDIME, CreateDIMETestFunc, DIMETestFuncMarginalPDF
 
 @doc raw"""
-    DIMESampler(lprobFunc::Function, init::Array, niter::Int; sigma::Float64=1e-5, gamma=nothing, aimh_prob::Float64=0.05, nsamples_proposal_dist=nothing, df_proposal_dist::Int=10, delta::Float64=.999, progress::Bool=true)
+    DIMESampler(lprobFunc::Function, init::Array, niter::Int; sigma::Float64=1e-5, gamma=nothing, aimh_prob::Float64=0.05, nsamples_proposal_dist=nothing, df_proposal_dist::Int=10, rho::Float64=.999, progress::Bool=true)
 
 # Arguments
 - `lprobFunc::Function`: the likelihood function to be sampled. Expected to be vectorized.
@@ -20,10 +20,10 @@ export RunDIME, CreateDIMETestFunc, DIMETestFuncMarginalPDF
 - `sigma::Float=1e-5`: the standard deviation of the Gaussian used to stretch the proposal vector.
 - `gamma::Float=nothing`: the mean stretch factor for the proposal vector. By default, it is ``2.38 / \sqrt{2\,\mathrm{ndim}}`` as recommended by `ter Braak (2006) <http://www.stat.columbia.edu/~gelman/stuff_for_blog/cajo.pdf>`_.
 - `aimh_prob::Float=0.1`: the probability to draw a AIMH proposal. 
-- `delta::Float=0.999`: the decay parameter for mean and covariance of the AIMH proposals.
+- `rho::Float=0.999`: the decay parameter for mean and covariance of the AIMH proposals.
 - `df_proposal_dist::Float=10`: the degrees of freedom of the multivariate t distribution used for AIMH proposals.
 """
-function RunDIME(lprobFunc::Function, init::Array, niter::Int; sigma::Float64=1e-5, gamma=nothing, aimh_prob::Float64=0.1, df_proposal_dist::Int=10, delta::Float64=.999, progress::Bool=true)
+function RunDIME(lprobFunc::Function, init::Array, niter::Int; sigma::Float64=1e-5, gamma=nothing, aimh_prob::Float64=0.1, df_proposal_dist::Int=10, rho::Float64=.999, progress::Bool=true)
 
     ndim, nchain = size(init)
     isplit = nchain ÷ 2
@@ -81,7 +81,7 @@ function RunDIME(lprobFunc::Function, init::Array, niter::Int; sigma::Float64=1e
         statelweight = cumlweight - newcumlweight
         ccov = exp(statelweight) * ccov + exp(lweight - newcumlweight) * ncov
         cmean = exp(statelweight) * cmean + exp(lweight - newcumlweight) * nmean
-        cumlweight = newcumlweight + log(delta)
+        cumlweight = newcumlweight + log(rho)
         naccepted = 0
 
         # must iterate over current and reference ensemble
